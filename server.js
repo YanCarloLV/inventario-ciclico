a6f215602be7e59a7de3edc16b77b51c42560b7b
YanCarloLV <axelyan2@gmail.com>
2026-03-08 02:47:39 -0600

    Agregando el frontend oficial

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // CONEXIÓN A MONGODB
-const MONGO_URI = process.env.MONGO_URI;
+const mongoURI = "mongodb+srv://admin:Autodesk1234@inventario-ciclico.6ntlqbn.mongodb.net/?appName=Inventario-Ciclico";
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log("🚀 Conectado a MongoDB Atlas"))
